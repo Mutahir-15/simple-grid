a4ccefae4e45057bcd5d5d5fa5fb7dd45c12564a
Mutahir-15 <mutahir.bin.athar2005@gmail.com>
2024-12-26 01:12:58 +0500

    Updated

---
+++ src/components/Grid.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Grid() {
+  return (
+    <div className='gap-4'>
+      <div className="bg-yellow-400">Header</div>
+      <div className="bg-yellow-400">Nav</div>
+      <div className="bg-yellow-400">Article</div>
+      <div className="bg-yellow-400">Ads</div>
+      <div className="bg-yellow-400">Footer</div>
+    </div>
+  )
+}
+
+export default Grid
