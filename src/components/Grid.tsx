53f70c190c4503ed5436058cc1ea9a577861770f
Mutahir-15 <mutahir.bin.athar2005@gmail.com>
2024-12-26 16:56:48 +0500

    Update

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 function Grid() {
   return (
-    <div className='gap-4'>
+    <main className='max-w-[1440px] mx-auto'>
+    <div className='justify-center items-center'>
       <div className="bg-yellow-400">Header</div>
-      <div className="bg-yellow-400">Nav</div>
+      <div className="bg-yellow-400">SideBar</div>
       <div className="bg-yellow-400">Article</div>
       <div className="bg-yellow-400">Ads</div>
       <div className="bg-yellow-400">Footer</div>
     </div>
+    </main>
   )
 }
 
